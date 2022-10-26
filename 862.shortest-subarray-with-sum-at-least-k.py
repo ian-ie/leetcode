152511735fd007b776ae943757cede1e48498533
ianie <2817181586@qq.com>
2022-10-27 00:28:58 +0800

    22/10/26

---
+++ 862.shortest-subarray-with-sum-at-least-k.py
@@ -0,0 +1,9 @@
+#
+# @lc app=leetcode.cn id=862 lang=python3
+#
+# [862] 和至少为 K 的最短子数组
+#
+# @lc code=start
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+   # @lc code=end
