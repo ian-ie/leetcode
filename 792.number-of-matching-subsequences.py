df6ae2354f3dfc76b9f0e84c3d4b1e329963b01c
ianie <2817181586@qq.com>
2022-11-20 22:53:50 +0800

    medium 分桶

---
+++ 792.number-of-matching-subsequences.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=792 lang=python3
+#
+# [792] 匹配子序列的单词数
+#
+
+# @lc code=start
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        d = defaultdict(deque)
+        for i, w in enumerate(words):
+            d[w[0]].append((i, 0))
+        ans = 0
+        for c in s:
+            for _ in range(len(d[c])):
+                i, j = d[c].popleft()
+                j += 1
+                if j == len(words[i]):
+                    ans += 1
+                else:
+                    d[words[i][j]].append((i, j))
+        return ans
+
+
+# @lc code=end
