a06f6efab3388db56db933571322b7ff3d9883de
ianie <2817181586@qq.com>
2022-12-03 17:32:55 +0800

    easy 遍历

---
+++ 1796.second-largest-digit-in-a-string.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=1796 lang=python3
+#
+# [1796] 字符串中第二大的数字
+#
+
+# @lc code=start
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        first = second = -1
+        for c in s:
+            if c.isdigit():
+                num = int(c)
+                if num > first:
+                    second = first
+                    first = num
+                elif second < num < first:
+                    second = num
+        return second
+
+
+# @lc code=end
